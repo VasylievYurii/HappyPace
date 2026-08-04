b5a61d3cf21348daf51f439c76b6a843924967a7
VasylievYurii <uraplanetianin@gmail.com>
2026-08-05 00:54:20 +0300

    install swiper

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -1,3 +1,13 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Імпортуємо модулі, які додають функціонал (навігація, пагінація)
+import { Navigation, Pagination } from 'swiper/modules';
+
+// Обов'язково імпортуємо стилі
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const HeroCarousel = () => {
   return (
     <section style={{ padding: '2rem', border: '1px solid blue' }}>
